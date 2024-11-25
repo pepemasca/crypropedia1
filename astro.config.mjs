3a33627fd8862fde4c7f929bbaec59c778640176
pepemasca <144556682+pepemasca@users.noreply.github.com>
2024-11-25 10:40:41 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://github.com/pepemasca/crypropedia1", // Cambia este a tu sitio si lo necesitas
-  base: "/cryptopedia1", // Cambia "<nombre-del-repo>" al nombre de tu repositorio
+  base: "/crypropedia1", // Cambia "<nombre-del-repo>" al nombre de tu repositorio
   output: "static", // Asegura que genere un sitio estático
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
